9cb1ae9a71a598c9228979a0bda66b919cfbb8cc
star <502545703@qq.com>
2020-01-07 18:45:44 +0800

    update

--- 超算/hunan-egg/config.default.js
+++ 超算/hunan-egg/config.default.js
@@ -72,21 +72,14 @@ module.exports = appInfo => {
   };
 
   exports.mysql = {
-    // 单数据库信息配置
     client: {
       host: '192.168.32.201',
-      // 端口号
       port: '3306',
-      // 用户名
       user: 'root',
-      // 密码
       password: 'esri@123',
-      // 数据库名
       database: 'cloud_hunan',
     },
-    // 是否加载到 app 上，默认开启
     app: true,
-    // 是否加载到 agent 上，默认关闭
     agent: false,
   };
 
@@ -105,14 +98,14 @@ module.exports = appInfo => {
   };
 
   // redis
-  // config.redis = {
-  //   client: {
-  //     port: 6379,          // Redis port
-  //     host: '192.168.32.167',   // Redis host
-  //     password: 'esri@123',
-  //     db: 0
-  //   }
-  // };
+  config.redis = {
+    client: {
+      port: 6379,          
+      host: '192.168.32.167',   
+      password: 'esri@123',
+      db: 0
+    }
+  };
 
   //s3
   config.s3 = {
